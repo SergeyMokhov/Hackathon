fbc0038e5768e56c9ef32c0cfdc94594eefdcf09
OFFICE\jocol <jocol@pelmorex.com>
2018-11-22 12:56:23 -0500

    DIAD-1: Web application hack

---
+++ VS/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "GET OUT";
+
+            return View();
+        }
+
+        public JsonResult GetSearchValue(string search)
+        {
+            List<String> places = new List<string>(new String[] { "windsor", "milton", "mississauga", "toronto", "thornhill", "oakville", "vaughn" });
+            IEnumerable<String> p = places.Where(x => x.StartsWith(search) || search == null).ToList();
+            List<PlaceModel> allsearch = p.Select(x => new PlaceModel
+            {
+                Name = x
+            }).ToList();
+            return Json(allsearch, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VS/WebApplication/Models/ResultModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class ResultModel
+    {
+        public string Name { get; set; }
+        public string Activity { get; set; }
+        public string Traffic { get; set; }
+        public string Reviews { get; set; }
+        public string Wind { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Sunrise { get; set; }
+
+        public string Sunset { get; set; }
+    }
+}
